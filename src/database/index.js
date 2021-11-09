a82a23422d3eb2ac67e3cd6ab683bbd222e6503b
Foliaco <88067658+Foliaco@users.noreply.github.com>
2021-11-08 22:56:29 -0500

    database

--- src/database/index.js
+++ src/database/index.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 const mysql=require('mysql');
 //mysql://:@/?reconnect=true
 const coneccion=mysql.createConnection({
-                                host:'us-cdbr-east-04.cleardb.com',
-                                database:'heroku_16ceaac458aabac',
-                                user:'b7aed29a3eb1dd',
-                                password:'716836f2'})
+                                host:'uyu7j8yohcwo35j3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+                                database:'p0pazym23ybflqyi',
+                                user:'haslw3bgqtomglji',
+                                password:'wl4fypw7lg1f40ud'})
 
 coneccion.connect(err=>{
     if(err){
