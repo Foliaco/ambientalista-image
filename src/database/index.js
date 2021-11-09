c6bb824000a6d5297468a5937884f0001e3e8b06
Foliaco <88067658+Foliaco@users.noreply.github.com>
2021-11-08 23:07:41 -0500

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 const mysql=require('mysql');
 //mysql://:@/?reconnect=true
 const coneccion=mysql.createConnection({
-                                host:'uyu7j8yohcwo35j3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-                                database:'p0pazym23ybflqyi',
-                                user:'haslw3bgqtomglji',
-                                password:'wl4fypw7lg1f40ud'})
+                                host:'https://db4free.net',
+                                database:'crudfoliaco',
+                                user:'miguelfoliaco',
+                                password:'Mr.zektalol'})
 
 coneccion.connect(err=>{
     if(err){
